df0e2445264c45b9f3bb6977f6af59824913cd5e
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-07-31 19:41:18 +0300

    burger

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,37 @@
+(() => {
+  const mobileMenu = document.querySelector('.header-menu');
+  const openMenuBtn = document.querySelector('.open-menu');
+  const closeMenuBtn = document.querySelector('.close-btn');
+
+ 
+  openMenuBtn.addEventListener('click', function() {
+    mobileMenu.classList.toggle('is-open');
+    openMenuBtn.style.display = 'none';
+    closeMenuBtn.style.display = 'inline-block';
+});
+
+  closeMenuBtn.addEventListener('click', function() {
+    mobileMenu.classList.toggle('is-open');
+    closeMenuBtn.style.display = 'none';
+    openMenuBtn.style.display = 'inline-block';
+});
+
+
+
+
+  const menuLinks = document.querySelectorAll('.header-list-link');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      
+    });
+  });
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 1440px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+  });
+})();
