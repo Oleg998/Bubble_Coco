8ab21cf97373dffb700d2354a8e1e8ff3385c6c0
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-08-03 21:21:19 +0300

    footer

---
+++ src/js/slider.js
@@ -0,0 +1,28 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css/bundle'
+
+    const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 8,
+    loop: true,
+    speed: 600,
+  
+    modules: [Navigation, Keyboard],
+    navigation: {
+      nextEl: '.open-right',
+      prevEl: '.open-left',
+    },
+  
+    keyboard: {
+      enabled: true,
+    },
+  
+    breakpoints: {
+      1440: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+    },
+  });
