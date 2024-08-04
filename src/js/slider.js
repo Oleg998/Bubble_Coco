1255b866db89eb36dc90b309ea357820c5afa275
Oleg998 <trussameto@gmail.com>
2024-08-05 02:39:46 +0300

    firs_fix

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,8 +5,12 @@ import { Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css/bundle'
 
     const swiper = new Swiper('.swiper', {
+    
     direction: 'horizontal',
+    centeredSlides: true,
     slidesPerView: 1,
+    slidesPerGroup:1,
+
     spaceBetween: 8,
     loop: true,
     speed: 600,
@@ -23,8 +27,25 @@ import 'swiper/css/bundle'
   
     breakpoints: {
       1440: {
-        slidesPerView: 1,
+        slidesPerView: 3,
         spaceBetween: 16,
       },
+      on: {
+        slideChangeTransitionStart: function () {
+          this.slides.forEach((slide) => {
+            slide.style.transform = 'scale(1)';
+          });
+          this.slides[this.activeIndex].style.transform = 'scale(1.33)';
+        },
+        init: function () {
+          this.slides.forEach((slide) => {
+            slide.style.transition = 'transform 0.6s';
+          });
+          this.slides[this.activeIndex].style.transform = 'scale(1.33)';
+        },
+      },
     },
+
+   
+
   });
